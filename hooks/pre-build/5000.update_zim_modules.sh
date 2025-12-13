43613b93cc658e4d8087b6cf62d386da57981417
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-13 13:11:58 +0800

    1.0.6

---
+++ hooks/pre-build/5000.update_zim_modules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+. "$KAM_HOOKS_ROOT/lib/utils.sh"
+
+require_command which "which not found"
+require_command zsh "zsh not found"
+
+export HOME=$KAM_MODULE_ROOT
+if [ "$KAM_INTERACTIVE" = "1" ]; then
+    log_info "Starting interactive shell"
+    exec $(which zsh) -i
+else
+    # 执行zimfw update
+    log_info "Starting zimfw update"
+    $(which zsh) -c "source $HOME/.zshrc && which git && zimfw update && zimfw upgrade"
+fi
--- src/asl/action.sh
+++ src/asl/action.sh
@@ -1,6 +1,7 @@
 MODDIR=${0%/*}
 echo "这个简单的脚本用于注释/取消注释每次进入rurima环境时候的提示信息。"
 echo "This simple script is used to comment out/uncomment the prompt message each time you enter the rurima environment."
+sleep 3
 
 f="$MODDIR/.zshrc"
 sed -i '1{s/^#//;t;s/^/#/}' "$f"
